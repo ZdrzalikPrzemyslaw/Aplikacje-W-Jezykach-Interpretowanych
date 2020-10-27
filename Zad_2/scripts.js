a11a05074c69125b59610002d402fe9257ffe475
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-10-27 11:08:59 +0100

    change

--- Zad_2/scripts.js
+++ Zad_2/scripts.js
@@ -1,14 +1,13 @@
 "use strict";
-var todo_item = /** @class */ (function () {
-    function todo_item(title, description, place, dueDate) {
+class todo_item {
+    constructor(title, description, place, dueDate) {
         this.title = title;
         this.description = description;
         this.place = place;
         this.dueDate = dueDate;
     }
-    return todo_item;
-}());
-var todoList = [];
+}
+let todoList = [];
 $.ajax({
     // copy Your bin identifier here. It can be obtained in the dashboard
     url: 'https://api.jsonbin.io/b/5f7f5a7365b18913fc5ccf2c/latest',
@@ -16,15 +15,15 @@ $.ajax({
     headers: {
         'secret-key': "$2b$10$NTKfR.Ut9Z8el6x6WakuV.yUvORzT5RI.w3yTnSLcHInw6elnEB3e"
     },
-    success: function (data) {
+    success: (data) => {
         console.log(data);
         todoList = data;
     },
-    error: function (err) {
+    error: (err) => {
         console.log(err.responseJSON);
     }
 });
-var updateJSONbin = function () {
+let updateJSONbin = function () {
     $.ajax({
         url: 'https://api.jsonbin.io/b/5f7f5a7365b18913fc5ccf2c',
         type: 'PUT',
@@ -34,41 +33,41 @@ var updateJSONbin = function () {
         },
         contentType: 'application/json',
         data: JSON.stringify(todoList),
-        success: function (data) {
+        success: (data) => {
             console.log(data);
         },
-        error: function (err) {
+        error: (err) => {
             console.log(err.responseJSON);
         }
     });
 };
-var updateTodoList = function () {
-    var deleteTodo = function (index) {
+let updateTodoList = function () {
+    let deleteTodo = function (index) {
         todoList.splice(index, 1);
         updateTodoList();
         updateJSONbin();
     };
-    var todoListView = $("#todoListView")[0];
+    let todoListView = $("#todoListView")[0];
     //remove all elements
     while (todoListView.firstChild) {
         todoListView.removeChild(todoListView.firstChild);
     }
-    var filterInputDesc = $("#inputSearch")[0];
-    var filterInputDate1 = $("#inputSearchDate1")[0];
-    var filterInputDate2 = $("#inputSearchDate2")[0];
-    var dateStart = new Date(filterInputDate1.value);
-    var dateEnd = new Date(filterInputDate2.value);
+    let filterInputDesc = $("#inputSearch")[0];
+    let filterInputDate1 = $("#inputSearchDate1")[0];
+    let filterInputDate2 = $("#inputSearchDate2")[0];
+    let dateStart = new Date(filterInputDate1.value);
+    let dateEnd = new Date(filterInputDate2.value);
     // add all elementsD
-    var containing_table = $('<table>').addClass("table");
+    let containing_table = $('<table>').addClass("table");
     {
-        var thead = $('<thead></thead>').addClass("thead-dark").appendTo(containing_table);
-        var tr = $('<tr></tr>').appendTo(thead);
-        for (var item in todoList[0]) {
+        let thead = $('<thead></thead>').addClass("thead-dark").appendTo(containing_table);
+        let tr = $('<tr></tr>').appendTo(thead);
+        for (let item in todoList[0]) {
             $('<th></th>').appendTo(tr).text(item);
         }
         $('<th></th>').appendTo(tr).text("remove");
     }
-    var _loop_1 = function (todo) {
+    for (let todo in todoList) {
         if ((filterInputDesc.value == "") ||
             (todoList[todo].title.includes(filterInputDesc.value)) ||
             (todoList[todo].description.includes(filterInputDesc.value))) {
@@ -76,16 +75,16 @@ var updateTodoList = function () {
                 || ((dateStart <= new Date(todoList[todo].dueDate)) && (filterInputDate2.value == ""))
                 || ((dateEnd >= new Date(todoList[todo].dueDate)) && (filterInputDate1.value == ""))
                 || ((dateEnd >= new Date(todoList[todo].dueDate)) && (dateStart <= new Date(todoList[todo].dueDate)))) {
-                var tr = $('<tr></tr>').appendTo(containing_table);
-                var newDeleteButton = $('<input></input>').attr({
+                let tr = $('<tr></tr>').appendTo(containing_table);
+                let newDeleteButton = $('<input></input>').attr({
                     type: 'button',
-                    value: 'x'
+                    value: 'x',
                 });
-                var onClickFun = function () { deleteTodo(todoList.indexOf(todo)); };
+                let onClickFun = function () { deleteTodo(todoList.indexOf(todo)); };
                 newDeleteButton.click(onClickFun);
-                for (var item in todoList[todo]) {
+                for (let item in todoList[todo]) {
                     if (Date.parse(todoList[todo][item])) {
-                        var date = new Date(todoList[todo][item]);
+                        let date = new Date(todoList[todo][item]);
                         $('<td></td>').appendTo(tr).text(date.toDateString());
                     }
                     else {
@@ -95,20 +94,17 @@ var updateTodoList = function () {
                 $('<td></td>').appendTo(tr).append(newDeleteButton);
             }
         }
-    };
-    for (var todo in todoList) {
-        _loop_1(todo);
     }
     if (todoList.length != 0)
         containing_table.appendTo(todoListView);
     // todoListView.appendChild(containing_table);
 };
-var addTodo = function () {
-    var inputTitle = $("#inputTitle")[0].value;
-    var inputDescription = $("#inputDescription")[0].value;
-    var inputPlace = $("#inputPlace")[0].value;
-    var inputDate = $("#inputDate")[0].value;
-    var newTodo = new todo_item(inputTitle, inputDescription, inputPlace, new Date(inputDate));
+let addTodo = function () {
+    let inputTitle = $("#inputTitle")[0].value;
+    let inputDescription = $("#inputDescription")[0].value;
+    let inputPlace = $("#inputPlace")[0].value;
+    let inputDate = $("#inputDate")[0].value;
+    let newTodo = new todo_item(inputTitle, inputDescription, inputPlace, new Date(inputDate));
     todoList.push(newTodo);
     updateTodoList();
     updateJSONbin();
