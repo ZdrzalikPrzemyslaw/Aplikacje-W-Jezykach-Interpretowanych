6ec5a2f9706279e801dde437a2a4b9d0ef556e67
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-11-20 21:11:37 +0100

    start work

---
+++ Zad_3/zad_3/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MovieTable msg="XD"/>
+  </div>
+</template>
+
+<script>
+import MovieTable from './components/MovieTable.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    MovieTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Zad_3/zad_3/src/components/MovieTable.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="my_table">
+    <table>
+      <thead>
+        <tr>
+          <th>
+            Title
+          </th>
+          <th>
+            Production Year
+          </th>
+          <th>
+            Cast
+          </th>
+          <th>
+            Genres
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="movie in this.get_movies()" :key="movie.title">
+          <td>
+            {{ movie.title }}
+          </td>
+          <td>
+            {{ movie.year }}
+          </td>
+          <td>
+            {{ movie.cast }}
+          </td>
+          <td>
+            {{ movie.genres }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import json from "../json/movies.json";
+
+export default {
+  name: "MovieTable",
+  data() {
+    return {
+      movies: json,
+      movie_count: 10,
+      current_start_movie: 0,
+    };
+  },
+  methods: {
+    get_movies: function() {
+      return this.movies.slice(
+        this.current_start_movie,
+        this.current_start_movie + this.movie_count
+      );
+    },
+  },
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
